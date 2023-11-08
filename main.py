75cc502cbcdb26f91402e4c97ca68e0ec423d723
Blitz <jsnajera1017@gmail.com>
2023-11-08 17:50:48 -0600

    Implementation of AOIS and Orientation and Centered code

--- main.py
+++ main.py
@@ -3,11 +3,29 @@ import matplotlib.pylab as plt
 import numpy as np
 import pandas as pd
 
-img=cv.imread("IMG/36.PNG",0)
-#pd.Series(img_mpl.flatten()).plot(kind='hist', bins=50, title= "PixelDistribution")
-_, threshold=cv.threshold(img,20,250,cv.THRESH_BINARY)
+XCOORD_ORIENTATION=523
+YCOORD_ORIENTATION=40
+
+XCOORDMIN=150
+XCOORDMAX=470
+YCOORDMIN=90
+YCOORDMAX=380
+
+STANDARDCENTERX=174
+STANDARDCENTERY=144
+
+img_ori=cv.imread("IMG/36.PNG",1)
+img = cv.cvtColor(img_ori, cv.COLOR_BGR2GRAY)
+aoi=img[YCOORDMIN:YCOORDMAX,XCOORDMIN:XCOORDMAX]
+aoi_orientation=img[YCOORD_ORIENTATION:YCOORD_ORIENTATION+30,XCOORD_ORIENTATION:XCOORD_ORIENTATION+30]
+
+cv.imshow('asda',aoi)
+cv.imshow('asda',aoi_orientation)
+
+_, threshold=cv.threshold(aoi,20,250,cv.THRESH_BINARY)
 contours, _ = cv.findContours(threshold, cv.RETR_TREE, cv.CHAIN_APPROX_SIMPLE) 
-print(img[374,314])
+orientation_value=aoi_orientation[10,10]
+
 XYcontour=[]
 plt.imshow(threshold)
 
@@ -22,17 +40,29 @@ for contour in contours:
         if(len(i)<=3 and len(i)>0):
             i=int(i)
             XYcontour.append(i)
-
-    if XYcontour[0]<450 and XYcontour[0]>150 and XYcontour[1]<350 and XYcontour[1]>100:
         approx = cv.approxPolyDP(contour, 0.01 * cv.arcLength(contour, True), True)
         M = cv.moments(contour)
         if M['m00'] != 0.0:
             x = int(M['m10']/M['m00']) 
-            y = int(M['m01']/M['m00']) 
+            y = int(M['m01']/M['m00'])   
     XYcontour=[]
-cv.imshow('adsad',img)
+print("Centrado?:")
+if x<STANDARDCENTERX+10 and x>STANDARDCENTERX-10 and y>STANDARDCENTERY-10 and y<STANDARDCENTERY+10:
+    print("TRUE")
+else:
+    print("FALSE")
 print(x)
 print(y)
+
+print("Orientacion?")
+if orientation_value<20:
+    print("TRUE")
+else:
+    print("FALSE")
+print(orientation_value)
+
+cv.imshow('adsad',img)
+
 plt.show()
 #plt.show()
 
