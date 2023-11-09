fa628bbbb7b44c8fa766957bc2c6fe73a27451f5
Blitz <jsnajera1017@gmail.com>
2023-11-09 11:47:19 -0600

    Images copy and rename

--- Interfaz.py
+++ Interfaz.py
@@ -5,39 +5,19 @@ import shutil
 ventana = tkinter.Tk()
 ventana.geometry("600x350")
 
-def open_file():
-    curr_directory = os.getcwd()
-    file_path = filedialog.askopenfilename(initialdir=curr_directory, title="Select Image", filetypes=[('Image Files', '*.jpg')])
-    print(file_path)
-    return (file_path)
-    '''
-    archivo = filedialog.askopenfilename(title="abrir", initialdir = "C:/")
-    normal_image = list(archivo)
-    reversed_image = list(reversed(normal_image))
-    list_name_image = []
-    if ((reversed_image[0]) == "g") and ((reversed_image[1]) == "n"):
-         for i in reversed_image:
-            if i != "/":
-                   list_name_image.append(i)
-            else:
-                break
-    name_image = "".join(list(reversed(list_name_image)))
-    return name_image'''
 
 def uploadFiles():
     curr_directory="./IMG"
-    file_name="test"
-    file_path = filedialog.askopenfilename(initialdir=curr_directory, title="Select Image", filetypes=[('Image Files', '*.jpg')])
+    file_path = filedialog.askopenfilename(initialdir=curr_directory, title="Select Image", filetypes=[('Image Files', '*.PNG')])
     shutil.copy(file_path,curr_directory)
-    
+    print(file_path)
+    names=file_path.split("/")
+    names[len(names)-1]=names[len(names)-1].replace(".png","")
+    print(names)
+    file_old= curr_directory+"/"+names[len(names)-1]+".png"
+    file_new= curr_directory+"/"+"toto"+".png"
+    os.rename(file_old, file_new)
 
-def open_image():
-    img = open_file() #Whatever the given image is
-    save_path = "/IMG"
-    file_name = "test"
-    complete_name = os.path.join(save_path, file_name)
-    uploadFiles()
-    print(img) 
 
 button_upload_img= tkinter.Button(ventana, text ="Upload", command = uploadFiles(),bg= "yellow").pack()
 
